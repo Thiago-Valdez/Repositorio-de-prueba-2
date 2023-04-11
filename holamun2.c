182f478891c6e9d69b9ec0990aa74596048e94c8
thiago <thiagovakdez@gmail.com>
2023-04-11 13:52:08 -0300

    agregue

---
+++ holamun2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("ingrese valor chistoso: ");
+	scanf ("%d", &valor1);
+	printf ("ingrese valor chistoso 2: ");
+	scanf ("%d", &valor2);
+	valor1=valor1+valor2;
+	printf ("%d", valor1);
+}
